186dbd95d101bcda427ec63cb231bb0aeebd0fce
parvifolia <parvifoli@gmail.com>
2020-07-05 16:04:03 +0300

    v0.2.1 : .babelrc & webpack configuration added

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+modules.export = {
+    module: {
+        rules : [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use : {
+                loader: "babel-loader"
+            }
+        }]
+    }
+}
