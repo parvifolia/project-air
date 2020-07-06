1766282f1762cdfd15ac21df9be24f8675eb7df5
parvifolia <parvifoli@gmail.com>
2020-07-06 19:24:43 +0300

    v0.6.3: user logout builded with knox_logout

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('api/user/register', RegisterAPI.as_view()),
     path('api/user/login', LoginAPI.as_view()),
     path('api/user/auth', UserAPI.as_view()),
+
+    # Destroy the current user TOKEN with knox logout
+    path('api/user/logout', knox_views.LogoutView.as_view(),name='knox_logout')
 ]
